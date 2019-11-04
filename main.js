0085357064d6a74a906a6f6cd7c225c270f70375
JamesSwatton <jamessswatton@gmail.com>
2019-11-04 13:03:28 +0000

    add TextCell class

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 // The 'rows' variable will hold an array of arrays, with each inner array representing
 // a row of cells.
 
+// Uses 'reduce' to compute the maximum height of an array of cells and wraps that in 'map'
+// in order to do it for all rows in the 'rows' array.
 function rowHeights(rows) {
     return rows.map(row => {
         return row.reduce((max, cell) => {
@@ -10,6 +12,11 @@ function rowHeights(rows) {
     });
 }
 
+// The second argument that 'map' takes is index of the current element. The first argument (_)
+// will not be used. By mapping over the elements in the first row and only using the mapping
+// functions second argument, 'colWidths' builds up an array with one element for every
+// column index. The call to 'reduce' runs over the outer 'rows' array for each index and
+// picks out the width of the widest cell at that index.
 function colWidths(rows) {
     return rows[0].map((_, i) => {
         return rows.reduce((max, row) => {
@@ -18,6 +25,8 @@ function colWidths(rows) {
     });
 }
 
+// This function uses an internal helper function 'drawRow' to draw all rows and then joins
+// them together with newline characters.
 function drawTable(rows) {
     var heights = rowHeights(rows);
     var widths = colWidths(rows);
@@ -58,6 +67,14 @@ TextCell.prototype.minWidth = function() {
 };
 TextCell.prototype.minHeight = function() {
     return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+    var result = [];
+    for (var i = 0; i < height; i++) {
+        var line = this.text[i] || "";
+        result.push(line + repeat(" ", width - line.length));
+    }
+    return result;
 };
 
 
