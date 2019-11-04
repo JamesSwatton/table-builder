c1e5e0adbb9fc906ee0f6ac183cd16414d165a4c
JamesSwatton <jamessswatton@gmail.com>
2019-11-04 12:20:01 +0000

    add drawTable function

--- main.js
+++ main.js
@@ -10,10 +10,46 @@ function rowHeights(rows) {
     });
 }
 
-function colWidth(rows) {
+function colWidths(rows) {
     return rows[0].map((_, i) => {
         return rows.reduce((max, row) => {
             return Math.max(max, row[i].minWidth());
         }, 0);
     });
 }
+
+function drawTable(rows) {
+    var heights = rowHeights(rows);
+    var widths = colWidths(rows);
+
+    function drawLine(blocks, lineNo) {
+        return blocks.map(block => {
+            return block[lineNo];
+        }).join(" ");
+    }
+
+    function drawRow(row, rowNum) {
+        var blocks = row.map((cell, colNum) => {
+            return cell.draw(widths[colNum], heights[rowNum]);
+        });
+        return blocks[0].map((_, lineNo) => {
+            return drawLine(blocks, lineNo);
+        }).join("\n");
+    }
+
+    return rows.map(drawRow).join("\n");
+}
+
+function TextCell(text) {
+    this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function() {
+    return this.text.reduce((width, line) => {
+        return Math.max(width, line.length);
+    }, 0);
+};
+TextCell.prototype.minHeight = function() {
+    return this.text.length;
+};
+
+
